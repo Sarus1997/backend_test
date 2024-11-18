270f3d16aa8fe533dc86aee0f05fd5d91a6efec8
S Suwannapaond <saharat.rus@gmail.com>
2024-11-18 10:20:53 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const database = require('./model/db.json');
+const cors = require('cors');
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello! Node.js");
+});
+
+app.get("/model", (req, res) => {
+    res.send(database);
+});
+
+app.get('/model/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = database.find(user => user.id === id);
+
+    if (user) {
+        res.json(user);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+app.post('/model/add', (req, res) => {
+    const newUser = {
+        id: database.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        website: req.body.website,
+        address: {
+            street: req.body.address?.street,
+            suite: req.body.address?.suite,
+            city: req.body.address?.city,
+            zipcode: req.body.address?.zipcode,
+            geo: {
+                lat: req.body.address?.geo?.lat,
+                lng: req.body.address?.geo?.lng
+            }
+        }
+    };
+
+    database.push(newUser);
+    res.status(201).json(newUser);
+});
+
+app.put('/model/update/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const user = database.find(user => user.id === id);
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        user.phone = req.body.phone || user.phone;
+        user.website = req.body.website || user.website;
+        user.address = {
+            street: req.body.address?.street || user.address.street,
+            suite: req.body.address?.suite || user.address.suite,
+            city: req.body.address?.city || user.address.city,
+            zipcode: req.body.address?.zipcode || user.address.zipcode,
+            geo: {
+                lat: req.body.address?.geo?.lat || user.address.geo.lat,
+                lng: req.body.address?.geo?.lng || user.address.geo.lng
+            }
+        };
+
+        res.json(user);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+app.delete('/model/delete/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const index = database.findIndex(user => user.id === id);
+
+    if (index !== -1) {
+        const deletedUser = database.splice(index, 1);
+        res.json(deletedUser[0]);
+    } else {
+        res.status(404).json({ message: "User not found" });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
+
+module.exports = app;
+
